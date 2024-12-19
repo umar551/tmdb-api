60fc8f6b0442625f507efd09e2bfdc103322ee36
omer <unaeem@visibility.one>
2024-12-19 19:50:29 +0300

    tmbd api implementation

--- src/modules/swagger/swagger.config.ts
+++ src/modules/swagger/swagger.config.ts
@@ -11,5 +11,5 @@ export function setupSwagger(app: INestApplication) {
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api-docs', app, document);
 }
